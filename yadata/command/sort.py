e98aedd273fae5ea55bb7e7a56dc8e92881bdb91
gejza <gejza@alanko>
2020-04-30 16:29:42 +0200

    restrict for sort

--- yadata/command/sort.py
+++ yadata/command/sort.py
@@ -33,6 +33,8 @@ class Sort(YadataCommand):
 
     arguments=(
         Argument("-k","--sort-key",action="append",help="either fieldname or ~fieldname"),
+        Argument("-m","--module",action="append",default=[],help="python module to import"),
+        Argument("-r","--restrict",action="store",help="sort only those records for which the RESTRICT python term evaluates to True, output them, then output the other records"),
     )
 
     def __init__(self,ns):
@@ -40,14 +42,34 @@ class Sort(YadataCommand):
 
         if not self.ns.sort_key:
             raise ParameterError("sort: no key(s) given")
+        self.mods={}
+        for m in self.ns.module:
+            self.mods[m]=__import__(m)
 
         self.cmp_keys=keys_to_cmp(self.ns.sort_key)
 
     def execute(self):
 
-        l=list(sane_yaml.load_all(sys.stdin))
+        l_other=[]
+        if self.ns.restrict:
+            l=[]
+            l_orig=list(sane_yaml.load_all(sys.stdin))
+            for rec in l_orig:
+                d=dict(rec)
+                d.update(self.mods)
+                d["_type"]=type(rec).__name__
+                tf=eval(self.ns.restrict,d)
+                if tf:
+                    l.append(rec)
+                else:
+                    l_other.append(rec)
+        else:
+            l=list(sane_yaml.load_all(sys.stdin))
         l.sort(key=cmp_to_key(self.cmp_keys))
         for d in l:
+            print("---")
+            sys.stdout.write(sane_yaml.dump(d))
+        for d in l_other:
             print("---")
             sys.stdout.write(sane_yaml.dump(d))
 
