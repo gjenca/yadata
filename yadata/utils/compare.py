a98504e49a8598ea9c8b7aa0b28820a44444bcd3
Gejza Jenca <gejza@alanko>
2020-05-30 11:00:30 +0200

    menej kodu

--- yadata/utils/compare.py
+++ yadata/utils/compare.py
@@ -1,24 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import locale
+from functools import total_ordering
 
 def cmp_to_key(mycmp):
     'Convert a cmp= function into a key= function'
+    @total_ordering
     class K:
         def __init__(self, obj, *args):
             self.obj = obj
         def __lt__(self, other):
             return mycmp(self.obj, other.obj) < 0
-        def __gt__(self, other):
-            return mycmp(self.obj, other.obj) > 0
         def __eq__(self, other):
             return mycmp(self.obj, other.obj) == 0
-        def __le__(self, other):
-            return mycmp(self.obj, other.obj) <= 0
-        def __ge__(self, other):
-            return mycmp(self.obj, other.obj) >= 0
-        def __ne__(self, other):
-            return mycmp(self.obj, other.obj) != 0
     return K
 
 def cmp(val1,val2):
