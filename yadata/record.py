2413ad802c4cf68afa7ca6d3f2aad3f2cc7acff9
Gejza Jenca <gejza@alanko>
2020-06-03 14:02:33 +0200

    zmena prototypu, bo takto je to lepsie pre testovanie

--- yadata/record.py
+++ yadata/record.py
@@ -91,12 +91,13 @@ class Record(dict,metaclass=MetaRecord):
 #    _one_to_many=[]
 
 
-    def __init__(self,d):
+    def __init__(self,*args,**kwargs):
+        d=dict(*args,**kwargs)
         if not self.is_my_type(d):
            raise TypeError(f"Yadata type of {d} is not {type(self).yadata_tag}")
+        dict.__init__(self,d)
         self.path=None
         self.dirty=False
-        dict.__init__(self,d)
 
     def __new__(cls,*args,**kwargs):
 
