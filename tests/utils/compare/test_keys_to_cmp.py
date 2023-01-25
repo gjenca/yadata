1b0c1a0be5d42dd791e60832f412dff7ab0d5a2b
Gejza Jenča <gejza.jenca@stuba.sk>
2023-01-25 22:24:13 +0100

    testy prerobit

--- tests/utils/compare/test_keys_to_cmp.py
+++ tests/utils/compare/test_keys_to_cmp.py
@@ -7,14 +7,14 @@ da1b1={'a':1,'b':1}
 
 def test_keys_to_cmp_one_key_a():
 
-    cmp_this=keys_to_cmp(['a'])
+    cmp_this=keys_to_cmp(('a'))
     assert cmp_this(da1b2,da2b1)<0
     assert cmp_this(da1b1,da1b1)==0
     assert cmp_this(da2b1,da1b1)>0
 
 def test_keys_to_cmp_two_keys_a_b():
 
-    cmp_this=keys_to_cmp(['a','b'])
+    cmp_this=keys_to_cmp(('a','b'))
     assert cmp_this(da1b2,da1b1)>0
     assert cmp_this(da1b1,da1b2)<0
     assert cmp_this(da1b1,da1b1)==0
@@ -24,7 +24,7 @@ def test_keys_to_cmp_two_keys_a_b():
 
 def test_keys_to_cmp_two_keys_a_neg_b():
 
-    cmp_this=keys_to_cmp(['a','~b'])
+    cmp_this=keys_to_cmp(('a','~b'))
     assert cmp_this(da1b2,da1b1)<0
     assert cmp_this(da1b1,da1b2)>0
     assert cmp_this(da1b1,da1b1)==0
@@ -33,7 +33,7 @@ def test_keys_to_cmp_two_keys_a_neg_b():
     assert cmp_this(da2b1,da1b1)>0
 
 def test_keys_to_cmp_two_keys_neg_a_neg_b():
-    cmp_this=keys_to_cmp(['~a','~b'])
+    cmp_this=keys_to_cmp(('~a','~b'))
     assert cmp_this(da1b2,da1b1)<0
     assert cmp_this(da1b1,da1b2)>0
     assert cmp_this(da1b1,da1b1)==0
